d62f242a158e1d5db53742bc914d1793de989b84
Hamzah Shabbir <57156954+hamzahshabbir96@users.noreply.github.com>
2022-06-08 14:05:57 +0200

    Update and rename splittraintest.py to preprocessdata.py

--- graphneuralnetwork/utils/preprocessdata.py
+++ graphneuralnetwork/utils/preprocessdata.py
@@ -1,29 +1,29 @@
-from utils import graphreader,dataset
+from utils import graphreader,dataset_reader
 import numpy as np
 import glob
 
-class split:
+class Preprocess:
     def __init__(self,path,testratio=0.2):
         self.filespaths = glob.glob(path+"//*graph.txt")
-        self.labelspaths = glob.glob(path+"//*label.txt")
+        self.critical_temperature_paths = glob.glob(path+"//*critical_temp.txt")
         self.featurespath = glob.glob(path+"//*features.txt")
         self.filelist=True 
         size = len(self.filespaths)
 
         self.trainindexend = size - int(testratio*size)
         
-    def gettraintest(self):
+
+    def get_data(self):
         trainpaths = {'data':self.filespaths[:self.trainindexend],
-                      'label':self.labelspaths[:self.trainindexend],
+                      'label':self.critical_temperature_paths[:self.trainindexend],
                       'features':self.featurespath[:self.trainindexend]}
         testpaths  = {'data':self.filespaths[self.trainindexend:],
-                      'label':self.labelspaths[self.trainindexend:],
+                      'label':self.critical_temperature_paths[self.trainindexend:],
                       'features':self.featurespath[self.trainindexend:]}
 
 
-        traindataset = dataset.graphdataset(trainpaths,self.filelist)
-        testdataset = dataset.graphdataset(testpaths,self.filelist)
+        traindataset = dataset_reader.graphdataset(trainpaths,self.filelist)
+        testdataset = dataset_reader.graphdataset(testpaths,self.filelist)
 
         return traindataset,testdataset
-
 

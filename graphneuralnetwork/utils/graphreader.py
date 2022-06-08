d3db73b7ecf9d540b1b7ae694fe51e75c2cf5bf9
Hamzah Shabbir <57156954+hamzahshabbir96@users.noreply.github.com>
2022-06-08 14:06:34 +0200

    Update graphreader.py

--- graphneuralnetwork/utils/graphreader.py
+++ graphneuralnetwork/utils/graphreader.py
@@ -3,36 +3,36 @@ import glob
 import os
 class graphreader:
     def __init__(self,filepath):
-        self.adj = np.loadtxt(filepath,skiprows=1)
-        self.nnodes = int(open(filepath).readline().rstrip())
+        self.adjacent_matrix = np.loadtxt(filepath,skiprows=1)
+        self.number_of_nodes = int(open(filepath).readline().rstrip())
         
     
     def printfile(self):
-        print(self.adj)
-        print(type(self.adj))
-        print(self.nnodes)
-        print(type(self.nnodes))
+        print(self.adjacent_matrix)
+        print(type(self.adjacent_matrix))
+        print(self.number_of_nodes)
+        print(type(self.number_of_nodes))
 
 class graphreaderbatch:
     def __init__(self,filespath,filelist=False):
         if filelist :
             self.filespaths = filespath['data']
             self.featurespath= filespath['features']
-            self.labelspaths = filespath['label']
+            self.critical_temperature_paths = filespath['label']
         else :
             self.filespaths = glob.glob(filespath+"//*graph.txt")
-            self.labelspaths = glob.glob(filespath+"//*label.txt")
+            self.critical_temperature_paths = glob.glob(filespath+"//*label.txt")
             self.featurespath = glob.glob(filespath+"//*features.txt")
        
-        self.numexamples = len(self.labelspaths)
-        self.adj = [] 
-        self.nnodes = []
-        self.labels = []
+        self.numexamples = len(self.critical_temperature_paths)
+        self.adjacent_matrix = [] 
+        self.number_of_nodes = []
+        self.critical_temperature_ = []
         self.features = []
         
         for i in range (len(self.filespaths)):
-            self.adj.append(np.loadtxt(self.filespaths[i],skiprows=1))
-            self.nnodes.append(int(open(self.filespaths[i]).readline().rstrip()))
+            self.adjacent_matrix.append(np.loadtxt(self.filespaths[i],skiprows=1))
+            self.number_of_nodes.append(int(open(self.filespaths[i]).readline().rstrip()))
             
         for i in range (len(self.featurespath)):
             #np.loadtxt(self.filespaths[i],skiprows=1)
@@ -40,8 +40,6 @@ class graphreaderbatch:
             #self.features.append(int(open(self.featurespath[i]).readline().rstrip()))
   
 
-        if len(self.labelspaths) != 0:
-            for i in range (len(self.labelspaths)):
-                self.labels.append(float(open(self.labelspaths[i]).readline().rstrip()))
-
-       
+        if len(self.critical_temperature_paths) != 0:
+            for i in range (len(self.critical_temperature_paths)):
+                self.critical_temperature_.append(float(open(self.critical_temperature_paths[i]).readline().rstrip()))
